e5fdc974851c622a613426fa6d3c24af98066109
Ootsby <damian.sinclair@gmail.com>
2020-10-22 14:30:00 +0100

    Improved method for setting command occurences.

--- src/chat_test_mock.rs
+++ src/chat_test_mock.rs
@@ -8,14 +8,22 @@ use twitchchat::FromIrcMessage;
 
 pub fn run(
   send_incoming_chat_message: Sender<ChatMessage>,
-  num_bot_users:u64,
-  initial_commands_delay:Duration,
-  min_command_interval_ms:u64,
-  max_command_interval_ms:u64
+  num_bot_users: u64,
+  initial_commands_delay: Duration,
+  min_command_interval_ms: u64,
+  max_command_interval_ms: u64,
 ) -> () {
   thread::spawn(move || {
-    let commands = ["fire", "fire", "sword", "sword", "sword", "snek", "snek", "heart"];
-    let mock_user_names:Vec<String> = (1..num_bot_users).map(|u| format!("test_bot{}",u)).collect();
+
+    //The commands we'll be selecting from and the number of times each will be in the selection pool
+    let command_occurences = [("fire", 2), ("sword", 5), ("snek", 3), ("heart", 1)];
+
+    //Generate command pool
+    let command_pool = command_occurences.iter().fold(Vec::<&str>::new(), |mut v, c|{ v.extend(vec![c.0;c.1].iter()); return v;});
+        
+    let mock_user_names: Vec<String> = (1..num_bot_users)
+      .map(|u| format!("test_bot{}", u))
+      .collect();
     let mut rng = rand::thread_rng();
 
     thread::sleep(initial_commands_delay);
@@ -24,7 +32,7 @@ pub fn run(
       let input = format!(
         ":{mock_user}!user@host PRIVMSG #mock_channel :#{command} {column}\r\n",
         mock_user = mock_user_names[rng.gen_range(0, mock_user_names.len())],
-        command = commands[rng.gen_range(0, commands.len())],
+        command = command_pool[rng.gen_range(0, command_pool.len())],
         column = rng.gen_range(0, 10)
       );
       let (_pos, msg_a) = twitchchat::irc::parse_one(&input).unwrap();
@@ -34,8 +42,9 @@ pub fn run(
         .send(ChatMessage::new(test_msg))
         .unwrap();
 
-      thread::sleep(Duration::from_millis(rng.gen_range(min_command_interval_ms, max_command_interval_ms)));
+      thread::sleep(Duration::from_millis(
+        rng.gen_range(min_command_interval_ms, max_command_interval_ms),
+      ));
     }
   });
-
 }
