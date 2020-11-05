250887529988b1a9fbd9efb464ad64a8c97a5bbc
Ootsby <damian.sinclair@gmail.com>
2020-11-05 15:06:31 +0000

    Added random commands to bot list.

--- src/lib.rs
+++ src/lib.rs
@@ -66,7 +66,7 @@ impl Default for RunConfig {
             test_command_occurences: &[],
             attach_to_twitch_channel: true
             // test_bot_chatters: 5,
-            // test_command_occurences: &[("fire", 1), ("sword", 1), ("snake", 1), ("heart", 1)],
+            // test_command_occurences: &[("fire", 1), ("sword", 1), ("snake", 1), ("heart", 1), ("rng", 1), ("rand", 1), ("random", 1)],
             // attach_to_twitch_channel: false,
         }
     }
